1ff32df376bf18705ecf1403268fa82a29b5f935
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2024-02-13 22:11:16 +0700

    PrepairingClassesAndIDE

---
+++ src/main/java/com/example/BackendDevelop/UserRole/StandardUser.java
@@ -0,0 +1,7 @@
+package com.example.BackendDevelop.UserRole;
+
+public class StandardUser {
+    protected String login;
+    protected String password;
+    protected String FullName;
+}
---
+++ src/main/java/com/example/BackendDevelop/UserRole/Student.java
@@ -0,0 +1,9 @@
+package com.example.BackendDevelop.UserRole;
+
+import com.example.BackendDevelop.KeyPackage.Key;
+
+public class Student extends StandardUser{
+    public void toBook(Key key){
+
+    }
+}
