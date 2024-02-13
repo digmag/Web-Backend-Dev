30a56f523900513c6759809bf254be730fd49477
digmag <68858199+digmag@users.noreply.github.com>
2024-02-13 20:22:49 +0700

    Add files via upload

---
+++ src/main/java/com/example/BackendDevelop/BackendDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendDevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevelopApplication.class, args);
+	}
+
+}
